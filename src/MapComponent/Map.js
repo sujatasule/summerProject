f64d0cb5535586d76b0e890cf12b538cac213f07
superv2018 <41586952+superv2018@users.noreply.github.com>
2019-06-10 10:41:05 +0300

    change maps code and added marker

--- src/MapComponent/Map.js
+++ src/MapComponent/Map.js
@@ -1,24 +1,52 @@
 import React, {Component} from 'react';
-import GoogleMap  from 'google-map-react';
+import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from 'react-google-maps';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+function Map() {
+  
+  
+    return (
+    <GoogleMap 
+    defaultZoom={14} 
+    defaultCenter={{ lat: 60.169857, lng: 24.938379 }}
+    >
+      <Marker
+      position={
+       { lat: 60.244960, 
+          lng: 24.989050
+      }
+      }
+    
+      />
+  
+        <InfoWindow
+        position={
+          { lat: 60.244960, 
+             lng: 24.989050
+         }
+         }
+        >
+          <div>Events details</div>
+          </InfoWindow>
+     
+      </GoogleMap>
+    );
+  }
+  
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 
-export default class Map extends Component {
+export default class App extends Component {
   render () {
     return (
-      <GoogleMap 
-     bootstrapURLKeys = {{ key: process.env.REACT_APP_API_KEY}}
-      zoom = {14}
-      style = {mapStyles}
-      center = {{
-        lat: 60.169857,
-        lng: 24.938379
-      }}
+      <div style={{ width: "100vw", height: "100vh" }}>
+      <WrappedMap 
+      googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
+      loadingElement={<div style={{height: "100%"}} />}
+      containerElement = {<div style={{height: "100%"}} />}
+      mapElement={<div style={{height: "100%"}} />}
       />
+  
+  </div>
     
   );
 }
