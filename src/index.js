6e0ae49c3dededc12143e198f66cbc51b40b6251
Rabindra-Manandhar <rabindra.manandhar@edu.bc.fi>
2019-06-15 10:58:19 +0300

    2nd commit
    after merging victor's files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
+// require('dotenv').config();
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
